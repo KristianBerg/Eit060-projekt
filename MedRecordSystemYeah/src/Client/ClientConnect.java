5e559154954a6ee9534be78ce299e1b8a3ec4173
KristianBerg <kristianberg.jobb@gmail.com>
2016-02-17 11:50:57 +0100

    Implemented basic socket connection

---
+++ MedRecordSystemYeah/src/Client/ClientConnect.java
@@ -0,0 +1,29 @@
+package Client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientConnect {
+	private Socket socket;
+	int port;
+	String ip = "130.235.136.24";
+
+	public static void main(String[] args) {
+		new ClientConnect();
+	}
+
+	public ClientConnect() {
+		port = 5678; // such random, wow
+		try {
+			System.out.println("Waiting for connection...");
+			socket = new Socket(ip, port);
+			System.out.println("Connected!");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
