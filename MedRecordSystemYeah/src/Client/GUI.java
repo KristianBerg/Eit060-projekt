8e7fcc071de8b32211ee4caa8c2073e708ce6105
pigmato <pigmato@gmail.com>
2016-02-17 14:52:15 +0100

    GUI

--- MedRecordSystemYeah/src/Client/GUI.java
+++ MedRecordSystemYeah/src/Client/GUI.java
@@ -5,37 +5,67 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
-public class GUI extends JFrame {
-    private JLabel labelUsername = new JLabel("username: ");
-	private	JPanel		topPanel;
+public class GUI extends JFrame implements ActionListener {
+    private JLabel labelUsername = new JLabel("  ID of a patient: ");
+    private JTextField textUsername;
+    private JButton buttonLogin = new JButton("Search");
+	private	JPanel	topPanel;
+	private	JPanel	insidePanel=new JPanel();
+	private ClientConnect client;
 
 
 public GUI(){
 	super("Record Manager");
 	
+	client = new ClientConnect(this);
+    String s = "Enter an ID";
+
 	topPanel = new JPanel();
+	textUsername = new JTextField(s, 15);
+	
+	
 	topPanel.setLayout( new BorderLayout() );	
 	JPanel mainPanel = new JPanel(new GridBagLayout());
+
+	buttonLogin.addActionListener(this); 
+
 	
     GridBagConstraints constraints = new GridBagConstraints();
     constraints.anchor = GridBagConstraints.WEST;
     constraints.insets = new Insets(10, 10, 10, 10);
     
     topPanel.setBorder(BorderFactory.createTitledBorder(
-            BorderFactory.createEtchedBorder(), ""));
+            BorderFactory.createEtchedBorder(), "Database browser"));
+    insidePanel.setPreferredSize(new Dimension(150,30));
+    insidePanel.setMaximumSize( insidePanel.getPreferredSize());
+    insidePanel.setMinimumSize( insidePanel.getPreferredSize());
     
+    insidePanel.add(textUsername);
+    JSplitPane pane2 = new JSplitPane( JSplitPane.VERTICAL_SPLIT, 
+            labelUsername, insidePanel );
+    JSplitPane pane = new JSplitPane( JSplitPane.HORIZONTAL_SPLIT, 
+            pane2, buttonLogin );
+   
     
+    topPanel.add(pane);
+
     
     constraints.gridx = 0;
     constraints.gridy = 0;  
@@ -85,5 +115,10 @@ public static void main(String[] args){
 	}
 	});
 
+}
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	client.sendToServer(textUsername.getText());
 }
 }
