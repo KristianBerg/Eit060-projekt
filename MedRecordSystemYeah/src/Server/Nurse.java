0389cd7b98f59e23fee57e6d433ceac2c64d9403
Marcus Ekelund <dic14mek@student.lu.se>
2016-02-17 12:04:18 +0100

    added constructor nurse

--- MedRecordSystemYeah/src/Server/Nurse.java
+++ MedRecordSystemYeah/src/Server/Nurse.java
@@ -1,5 +1,20 @@
 package Server;
 
-public class Nurse {
+public class Nurse implements User {
+	private String division;
+	private String name;
 
+
+	public Nurse(String name, String division){
+		this.name = name;
+		this.division = division;
+	}
+	
+	
+	
+	public Object getRecords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
