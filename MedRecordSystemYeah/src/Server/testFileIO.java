74d3716a453f999ddb351ca45af8c24794236566
pigmato <pigmato@gmail.com>
2016-02-22 16:04:23 +0100

    testing createRecords

--- MedRecordSystemYeah/src/Server/testFileIO.java
+++ MedRecordSystemYeah/src/Server/testFileIO.java
@@ -13,25 +13,14 @@ public class testFileIO {
 		
 		AccessManager am = new AccessManager("records.txt");
 //		am.dumpUsersAndRecords();
-		//am.modifyRecord(0, 0, "arn");
-		//am.dumpUsersAndRecords();
+		
+		//use one method /time 
+		testCreateRecord(am);
+		testCreateRecordDoctor(am);
+		testCreateRecordPatient(am);
+		testDelete(am);
 
-		//testDelete(am);
 		
-		/** creates record
-		*/
-		am.login("fruktegott", "bananer");
-	    String[] newRecord = new String[4];
-	    newRecord[0]="gun";
-	    newRecord[1]="per";
-	    newRecord[2]="ann";
-	    newRecord[3]="barn";
-//	    newRecord[4]="pA92we";
-	    am.createRecord(newRecord);
-		String recs = am.readAllRecords();
-		System.out.println(recs);
-		
-//		am.login("per", "Er9Raa");
 //		am.saveToFile();
 	
 	}
@@ -50,5 +39,55 @@ public class testFileIO {
 		am.login("soc", "pA95we");
 		status = am.deleteRecord(0);	
 		System.out.println(status);	
+	}
+	
+	/** Testing CreateRecord() AND realAllRecords() methods
+	 * goes as superuser with unlimited access
+	 * expected additional record as output
+	 * IF NOT already existing, then expected output "file is already existing"
+	 */
+	private void testCreateRecord(AccessManager am){
+		am.login("fruktegott", "bananer");
+	    String[] newRecord = new String[4];
+	    newRecord[0]="gun";
+	    newRecord[1]="per";
+	    newRecord[2]="ann";
+	    newRecord[3]="barn";
+	    am.createRecord(newRecord);
+		String recs = am.readAllRecords();
+		System.out.println(recs);
+	}
+	
+	/** Testing CreateRecord() AND realAllRecords() methods
+	 * goes as Doctor with 2 records
+	 * expected additional record as output, default 3
+	 * IF NOT already existing, then expected output "file is already existing"
+	 */
+	private void testCreateRecordDoctor(AccessManager am){
+		am.login("gun", "Ae88ha");
+	    String[] newRecord = new String[4];
+	    newRecord[0]="gun";
+	    newRecord[1]="per";
+	    newRecord[2]="ann";
+	    newRecord[3]="barn";
+	    am.createRecord(newRecord);
+		String recs = am.readAllRecords();
+		System.out.println(recs);
+	}
+	
+	/** Testing CreateRecord() AND realAllRecords() methods
+	 * goes as Patient
+	 * CreateRecord should fail, expected output is this.recods
+	 */
+	private void testCreateRecordPatient(AccessManager am){
+		am.login("ann", "pA92we");
+	    String[] newRecord = new String[4];
+	    newRecord[0]="gun";
+	    newRecord[1]="per";
+	    newRecord[2]="ann";
+	    newRecord[3]="barn";
+	    am.createRecord(newRecord);
+		String recs = am.readAllRecords();
+		System.out.println(recs);
 	}
 }
